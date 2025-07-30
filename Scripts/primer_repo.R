3858bb1f5380f2073da681e7ce73e2dada339bf3
Jesus Reyes <jesusreyes@usantotomas.edu.co>
2025-07-29 19:19:54 -0500

    clase_Data_frames

--- Scripts/primer_repo.R
+++ Scripts/primer_repo.R
@@ -1,93 +1,139 @@
-## data fraames - limpieza de datos
+## Data frames - Limpieza de datos
 
-nombres <- c("Juan","Maria") 
-edades <- c(14,15)
+nombres <- c("Juan", "María") 
+edades <- c(14, 15)
 aprobado <- c(TRUE, FALSE)
 
-# se convierte en texto
+# Se convierte en texto
 X <- cbind(nombres, edades, aprobado)
 
-
-##respeta el tipo de caracter ingresado
+## Respeta el tipo de carácter ingresado
 df <- data.frame(nombres, edades, aprobado)
 
 ## Descripción de los datos
 summary(df)
 
-##citar columnas
+## Citar columnas
 df$nombres
 
 ####
 #### Accediendo a datos, datos por defecto de R
 
-##consulta de data especifica
+## Consulta de datos específica
 head(iris)
 head(trees)
-#consulta de listados de datos
+
+# Consulta de listados de datos
 data()
 
-##trabajaremos con la base de datos infert
+## Trabajaremos con la base de datos infert
 Y <- infert
 
-##Columna Age
+## Columna age
 Y$age
 
 # Selecciona los valores de la columna 2
-Y[,2]
+Y[, 2]
 
-#Selecciona los valores de la fila 1
-Y[1,]
+# Selecciona los valores de la fila 1
+Y[1, ]
 
-#Selecciona los vbalores de la fila 1 y columna 2
-Y[1,2]
+# Selecciona los valores de la fila 1 y columna 2
+Y[1, 2]
 
-## crear nueva variable filtrada, toma los valores de la fila 1 a la 5 y de la columna 1 a la 3
+## Crear nueva variable filtrada: toma los valores de la fila 1 a la 5 y de la columna 1 a la 3
+y <- Y[1:5, 1:3]
 
-y<- Y[1:5,1:3]
-
-#Selecciono las columnas "Parity" y "case"
-
-Y[,c("parity","case")]
+# Selecciona las columnas "parity" y "case"
+Y[, c("parity", "case")]
 
 ## Crea una variable independiente llamada "case"
-case<- Y$case
+case <- Y$case
 
-#
+# 
 attach(Y)
-#filtra por case con la función
+
+# Filtra por case con la función
 case
 
 ######
-######
-#Agregr filas y columnas
+###### 
+# Agregar filas y columnas 
 ######
 ######
 
-# En este ejemplo se trabajara con la base de datos del sistemaa "women"
-
+# En este ejemplo se trabajará con la base de datos del sistema "women"
 head(women)
 
-#La guardamos en la variable W
-W<- women
+# La guardamos en la variable W
+W <- women
 
-## 1 libra= 0.454 kg
-## 1 pulgada = 0.0254 mts
+## 1 libra = 0.454 kg
+## 1 pulgada = 0.0254 m
 
-# Se calcula el valopr de la altura en cms y el valor del peso en kgs
-alturamts <- W$height*0.0254
-PesoKG <- W$weight*0.454
+# Se calcula el valor de la altura en metros y el valor del peso en kilogramos
+alturamts <- W$height * 0.0254
+PesoKG <- W$weight * 0.454
 
-#cbind  , c de "columns"
-#adicionamos las dos columnas creadas a la variable W 
+# cbind: "c" de "columns"
+# Adicionamos las dos columnas creadas a la variable W 
+W <- cbind(W, alturamts, PesoKG)
 
-W<- cbind(W, alturamts, PesoKG)
+# Crear una nueva fila
+fila <- c(35, 56, 123, 123)
 
-#crear una nueva fila
-fila <- c(35,56,123,123)
+# rbind agrega filas; se podría, en modo ejemplo, describir como "r" de "row"
+W <- rbind(W, fila)
+
+# Quitar filas
+W <- W[-c(16), ]
+
+## IMC 
+## Peso / altura^2
+
+# Se crea el IMC
+imc <- (W$PesoKG / (W$alturamts)^2)
+
+# Para este caso se agrega la columna IMC a W, en el DataFrame W
+W <- cbind(W, imc)
+
+colnames(W)
+
+# Se quitan las columnas 1, 2 y 5
+W <- W[, c(1, 2, 5)]
+
+######
+###### 
+# Ordenando y filtrando datos 
+######
+######
 
 
-# rbind agrega filas, se podria en modo ejemplo describir como "r de row"
-rbind(W, fila)
+head(iris)
+
+# Selección de columna 1 y 2
+I <- iris[, c(1, 2)]
+
+## Selección de orden
+i <- order(I$Sepal.Length)
+
+# En esta línea se aplica el orden 
+I2 <- I[i, ]
+
+# Creación de orden por orden indicado de columnas
+ii <- order(I$Sepal.Length, I$Sepal.Width)
+
+# Aplicación de nuevo orden
+I3 <- I[ii, ]
 
 
+# Filtrando DataFrames
 
+# Subconjunto dado por condiciones: filtración de peso entre 60 kg y 70 kg, lógica proporcional
+subset(W, PesoKG > 60 & PesoKG < 70)
+
+# Operador O: el peso es mayor de 60 kg o la altura es menor de 1.70 m
+subset(W, PesoKG > 60 | alturamts < 1.70)
+
+# Se realiza subset con referencia a las columnas que quiero mostrar
+subset(W, PesoKG > 60 | alturamts < 1.70, select = c(alturamts, imc))
